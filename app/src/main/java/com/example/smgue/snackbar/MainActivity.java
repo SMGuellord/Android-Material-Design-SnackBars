7033494e9ebfacef3bc5c3c8e249e833d3bc2c90
SMGuellord <muhiyasumba@gmail.com>
2018-12-15 12:06:57 +0200

    changed frameLayout to coordinatorLayout in MainActivity

--- app/src/main/java/com/example/smgue/snackbar/MainActivity.java
+++ app/src/main/java/com/example/smgue/snackbar/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.smgue.snackbar;
 
+
+
+
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,13 +11,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private FrameLayout rootLayout;
+    private CoordinatorLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
